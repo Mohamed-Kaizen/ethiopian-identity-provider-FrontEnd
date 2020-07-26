71ef8bd189daba561ba4819dc7e54106b984704e
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-26 15:07:44 +0300

    add accepted and awaited list update tailwind.css file

---
+++ src/components/profile/awaited_list.svelte
@@ -0,0 +1,23 @@
+<script>
+	import {fly} from "svelte/transition"
+</script>
+
+<div
+	in:fly="{{y: 200, duration: 2000}}"
+	class="h-106 w-full lg:w-1/2 mt-8 mr-6 py-2 flex-shrink-0 flex flex-col
+	bg-purple-300 rounded-lg text-white overflow-y-auto">
+
+	<h3 class="flex items-center pt-1 pb-1 px-8 text-lg font-bold capitalize">
+		<!-- Header -->
+
+		<span>Awaited</span>
+
+	</h3>
+
+	<div class="flex flex-col items-center mt-12">
+
+		<img src="empty.png/" alt="empty awaited list" />
+
+	</div>
+
+</div>
--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,11 +1,31 @@
 <script>
+	import {fly} from "svelte/transition"
+
 	import {user} from "../../store.js"
+	import AcceptedList from "../../components/profile/accepted_list.svelte"
+	import AwaitedList from "../../components/profile/awaited_list.svelte"
 </script>
 
-<div class="flex flex-col capitalize text-3xl dark:text-gray-400">
+<div
+	in:fly="{{y: -200, duration: 1000}}"
+	class="flex flex-col capitalize text-3xl dark:text-gray-400">
 
 	<span class="font-semibold">hello,</span>
 
 	<span>{$user.username}!</span>
+
+</div>
+
+<h3
+	in:fly="{{x: -200, duration: 1000}}"
+	class="mt-8 text-xl font-semibold capitalize">
+	businesses
+</h3>
+
+<div class="flex flex-col lg:flex-row">
+
+	<AcceptedList />
+
+	<AwaitedList />
 
 </div>
